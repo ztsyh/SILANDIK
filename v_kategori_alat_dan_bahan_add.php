17e953eb6ac5c86d4b2f109aabf307482558ac9f
tiffanyovilia <64713851+tiffanyovilia@users.noreply.github.com>
2020-05-29 14:51:08 +0700

    Create v_kategori_alat_dan_bahan_add.php

---
+++ v_kategori_alat_dan_bahan_add.php
@@ -0,0 +1,51 @@
+\
+
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
+
+<div class="box-body big">
+    <?php echo form_open('',array('name'=>'faddmenugrup','class'=>'form-horizontal','role'=>'form'));?>
+        
+        
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Nama Alat dan Bahan</label>
+            <div class="col-sm-8">
+            <?php echo form_input(array('name'=>'nama_alat_dan_bahan','class'=>'form-control'));?>
+            <?php echo form_error('nama_alat_dan_bahan');?>
+            <span id="check_data"></span>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Kategori</label>
+            <div class="col-sm-8">
+            <?php echo form_input(array('name'=>'kategori','class'=>'form-control'));?>
+            <?php echo form_error('kategori');?>
+            <span id="check_data"></span>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Keterangan</label>
+            <div class="col-sm-8">
+            <?php echo form_input(array('name'=>'keterangan','class'=>'form-control'));?>
+            <?php echo form_error('keterangan');?>
+            <span id="check_data"></span>
+            </div>
+        </div>
+        
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Save</label>
+            <div class="col-sm-8 tutup">
+            <?php
+            echo button('send_form(document.faddmenugrup,"master/kategori_alat_dan_bahan/show_addForm/","#divsubcontent")','Save','btn btn-success')." ";
+            ?>
+            </div>
+        </div>
+    </form>
+</div>
+
+<script type="text/javascript">
+$(document).ready(function() {
+    $('.tutup').click(function(e) {  
+        $('#myModal').modal('hide');
+    });
+});
+</script>
