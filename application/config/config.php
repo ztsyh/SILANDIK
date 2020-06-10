40a19b43edbf095a8dd52f66b6e8d1ee77c8d291
tiffanyovilia <tiffanyovilia@students.unnes.ac.id>
2020-06-10 12:53:49 +0700

    mengubah base url menjadi dinamis

--- application/config/config.php
+++ application/config/config.php
@@ -1,7 +1,10 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 date_default_timezone_set('Asia/Bangkok');
-$config['base_url']            = "http://localhost/Laboratorium";
+
+$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+$config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
+$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 
 $config['index_page']          = '';
 
