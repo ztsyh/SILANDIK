ef69ae8c30e9ab65a01f838312e346966ade2b3e
tiffanyovilia <tiffanyovilia@students.unnes.ac.id>
2020-06-20 22:00:29 +0700

    view lokasi penyimpanan alat list

---
+++ application/views/kelola/lokasi_penyimpanan_alat/v_lokasi_penyimpanan_alat_list.php
@@ -0,0 +1,67 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
+
+    <div class="row" id="form_pembelian">
+      <div class="col-lg-12">
+        <div class="box box-primary">
+          <div class="box-header with-border">
+            <h3 class="box-title">Lokasi Penyimpanaan Alat</h3>
+
+            <div class="box-tools pull-right">
+            <?php
+              $sesi = from_session('level');
+              if ($sesi == '1' || $sesi == '2' || $sesi == '3' || $sesi == '6') {
+                echo button('load_silent("kelola/lokasi_penyimpanan_alat/form/base","#modal")','Tambah Data Lokasi','btn btn-success');
+              } else {
+                # code...
+              }
+              ?>
+            </div>
+          </div>
+          <div class="box-body">
+            <table width="100%" id="tableku" class="table table-striped">
+              <thead>
+                <th>No</th>
+                <th>ID Penyimpanan</th>
+                <th>Kode Alat</th>
+                <th>Kode Rak</th>
+                <th>Urutan</th>
+                <th>Act</th>
+              </thead>
+              <tbody>
+              <?php 
+          $i = 1;
+          foreach($lokasi_penyimpanan_alat->result() as $row): ?>
+          <tr>
+            <td align="center"><?=$i++?></td>
+            <td align="center"><?=$row->id?></td>
+            <td align="center"><?=$row->kode_alat?></td>
+            <td align="center"><?=$row->kode_rak?></td>
+            <td align="center"><?=$row->urutan?></td>
+            <td align="center">
+            <?php
+              $sesi = from_session('level');
+              if ($sesi == '1' || $sesi == '2' || $sesi == '3' || $sesi == '6') {
+                echo button('load_silent("kelola/lokasi_penyimpanan_alat/form/sub/'.$row->id.'","#modal")','','btn btn-info fa fa-edit','data-toggle="tooltip" title="Edit"');
+            } else {
+                # code...
+              }
+              ?>
+              <a href="<?= site_url('kelola/lokasi_penyimpanan_alat/delete/'.$row->id) ?>" class="btn btn-danger" onclick="return confirm('Anda yakin ingin menghapus data lokasi?')"><i class="fa fa-trash"></i></a>
+
+            </td>
+          </tr>
+        <?php endforeach;?>
+        </tbody>
+        </table>
+        <b>
+          <a href="<?php if(isset($_SERVER['HTTP_REFERER'])){echo $_SERVER['HTTP_REFERER'];}?>">Kembali</a>
+        </div>
+      </div>
+    </div>
+<script type="text/javascript">
+  $(document).ready(function() {
+    var table = $('#tableku').DataTable( {
+      "ordering": false,
+    } );
+  });
+</script>
