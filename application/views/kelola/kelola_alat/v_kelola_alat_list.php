1ccced6a8a4e80820e2aeb1df70847b561d8ccf8
tiffanyovilia <tiffanyovilia@students.unnes.ac.id>
2020-07-02 10:48:21 +0700

    views kelola alat

---
+++ application/views/kelola/kelola_alat/v_kelola_alat_list.php
@@ -0,0 +1,69 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
+
+    <div class="row" id="form_pembelian">
+      <div class="col-lg-12">
+        <div class="box box-danger">
+          <div class="box-header with-border">
+            <h3 class="box-title">Kelola Alat</h3>
+
+            <div class="box-tools pull-right">
+            <?php
+              $sesi = from_session('level');
+              if ($sesi == '1' || $sesi == '2' || $sesi == '4') {
+                echo button('load_silent("kelola/kelola_alat/form/base","#modal")','Tambah Nama Alat','btn btn-success');
+              } else {
+                # code...
+              }
+              ?>
+            </div>
+          </div>
+          <div class="box-body">
+            <table width="100%" id="tableku" class="table table-striped">
+              <thead>
+                <th>No</th>
+                <th>Nama Alat</th>
+                <th>Satuan</th>
+                <th>Status</th>
+                <th>Act</th>
+              </thead>
+              <tbody>
+          <?php 
+          $i = 1;
+          foreach($kelola_alat->result() as $row): ?>
+          <tr>
+            <td align="center"><?=$i++?></td>
+            <td align="center"><?=$row->nama_alat?></td>
+            <td align="center"><?=$row->satuan?></td>
+            <td align="center"><span class="badge bg-green">Ada</span></td>
+            <td align="center">
+            <?php
+              $sesi = from_session('level');
+              if ($sesi == '1' || $sesi == '2' || $sesi == '4') {
+                echo button('load_silent("kelola/kelola_alat/form/sub/'.$row->id.'","#modal")','','btn btn-info fa fa-edit','data-toggle="tooltip" title="Edit"');
+            ?><a href="<?= site_url('kelola/kelola_seri') ?>" class="btn btn-warning"><i class="fa fa-search"></i></a>
+              <?php
+              } else {
+                # code..
+                
+              }
+              ?>
+              <a href="<?= site_url('kelola/kelola_alat/delete/'.$row->id) ?>" class="btn btn-danger"
+              onclick="return confirm('Anda yakin ingin menghapus alat?')"><i class="fa fa-trash"></i></a>
+
+            </td>
+          </tr>
+
+        <?php endforeach;?>
+        </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+<script type="text/javascript">
+  $(document).ready(function() {
+    var table = $('#tableku').DataTable( {
+      "ordering": false,
+    } );
+  });
+</script>
