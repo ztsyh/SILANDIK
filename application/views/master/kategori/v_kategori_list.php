9d434db65050e2562ca053e440b33c9e70bcac76
tiffanyovilia <tiffanyovilia@students.unnes.ac.id>
2020-07-02 11:20:07 +0700

    views kategori alat bahan

---
+++ application/views/master/kategori/v_kategori_list.php
@@ -0,0 +1,61 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
+
+    <div class="row" id="form_pembelian">
+      <div class="col-lg-12">
+        <div class="box box-danger">
+          <div class="box-header with-border">
+            <h3 class="box-title">Master Kategori Alat & Bahan</h3>
+
+            <div class="box-tools pull-right">
+            <?php
+              $sesi = from_session('level');
+              if ($sesi == '1' || $sesi == '2') {
+                echo button('load_silent("master/kategori/form/base","#modal")','Tambah Kategori','btn btn-success');
+              } else {
+                # code...
+              }
+              ?>
+            </div>
+          </div>
+          <div class="box-body">
+            <table width="100%" id="tableku" class="table table-striped">
+              <thead>
+                <th>No</th>
+                <th>Kategori</th>
+                <th>Act</th>
+              </thead>
+              <tbody>
+              <?php 
+          $i = 1;
+          foreach($kategori->result() as $row): ?>
+          <tr>
+            <td align="center"><?=$i++?></td>
+            <td align="center"><?=$row->kategori?></td>
+            <td align="center">
+            <?php
+              $sesi = from_session('level');
+              if ($sesi == '1' || $sesi == '2') {
+                echo button('load_silent("master/kategori/form/sub/'.$row->id.'","#modal")','','btn btn-info fa fa-edit','data-toggle="tooltip" title="Edit"');
+            } else {
+                # code...
+              }
+              ?>
+              <a href="<?= site_url('master/kategori/delete/'.$row->id) ?>" class="btn btn-danger" onclick="return confirm('Anda yakin ingin menghapus kategori?')"><i class="fa fa-trash"></i></a>
+
+            </td>
+          </tr>
+        <?php endforeach;?>
+        </tbody>
+        </table>
+        <b>
+          <a href="<?php if(isset($_SERVER['HTTP_REFERER'])){echo $_SERVER['HTTP_REFERER'];}?>">Back</a>
+        </div>
+      </div>
+    </div>
+<script type="text/javascript">
+  $(document).ready(function() {
+    var table = $('#tableku').DataTable( {
+      "ordering": false,
+    } );
+  });
+</script>
